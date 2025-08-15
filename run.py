5b656506c53745730f21f0cf7291cd8b32c3566d
ullaskunder3 <ullaskunder3@gmail.com>
2025-08-15 23:44:55 +0530

    😁 veercel config for deployment

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
+# for development debug
+# app.config["DEBUG"] = False
 
 if __name__ == "__main__":
-    print("Open http://localhost:8080 in your browser")
-    app.run(port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    print(f"Running on http://localhost:{port}")
+    app.run(host="0.0.0.0", port=port)
